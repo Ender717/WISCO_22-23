a29e41af2ca0c7be7024d2201cd8f9c81b337198
Ender717 <73455333+Ender717@users.noreply.github.com>
2022-05-08 20:55:51 -0500

    Removed old .vs files

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,16 +1,6 @@
 #include "initialize.h"
 
 /**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-
-}
-
-/**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
@@ -34,7 +24,10 @@ void initialize()
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {}
+void disabled() 
+{
+
+}
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
