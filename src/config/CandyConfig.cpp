a5eb315b7963209b074755d5804fd917cdcfac92
Ender717 <nathan.sandvig@gmail.com>
2022-09-09 16:12:46 -0500

    candy tuning

--- src/config/CandyConfig.cpp
+++ src/config/CandyConfig.cpp
@@ -3,13 +3,13 @@
 namespace CandyConfig
 {
     // Constants
-    const double CATAPULT_COUNTS_PER_CYCLE = 5554.0;
+    const double CATAPULT_COUNTS_PER_CYCLE = 5400.0;
     const double CATAPULT_LOADED_POSITION = 2000.0;
 
     // Motor ports
-    const int LEFT_DRIVE_1_PORT = 11;
-    const int LEFT_DRIVE_2_PORT = 19;
-    const int RIGHT_DRIVE_1_PORT = 12;
-    const int RIGHT_DRIVE_2_PORT = 20;
-    const int CATAPULT_1_PORT = 16;
+    const int LEFT_DRIVE_1_PORT = 12;
+    const int LEFT_DRIVE_2_PORT = 20;
+    const int RIGHT_DRIVE_1_PORT = 3;
+    const int RIGHT_DRIVE_2_PORT = 10;
+    const int CATAPULT_1_PORT = 11;
 }
